273d553ee90c0fb1ade979ac3d4bd195517fbb99
ktyler292 <72824676+ktyler292@users.noreply.github.com>
2020-10-29 09:13:09 -0400

    Update UItesting.js

--- UItesting.js
+++ UItesting.js
@@ -3,44 +3,38 @@
 describe('Tests for STE Assessment', () => {
 
 
-    it('logs into the system with incorrect credentials, expecting error message', function () {
+    it('logs into the system with incorrect credentials, confirms error page loads', function () {
         const username = 'HG^^^&*H'
         const password = ']G_ce[24Ghd3'
         cy.visit('https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login')
         cy.contains('Username').type(username)
         cy.contains('Password').type(password)
         cy.get('form').submit()
-
-        cy.contains('.error-messages li','invalid username or password')
-
+        cy.contains('Error');
+        
     });
-    it('logs into the system with incorrect credentials,expecting error page to populate', function () {
-        const username = 'HG^^^&*H'
-        const password = ']G_ce[24Ghd3'
-        cy.visit('https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login')
-        cy.contains('Username').type(username)
-        cy.contains('Password').type(password)
-        cy.get('form').submit()
-        cy.contains('Error')
 
-    });
-    it('logs into the system with correct credentials', function () {
+    it('logs into the system with correct credentials, confirms correct page was loaded and an existing employee is found within table', function () {
         const username = 'TestUser32'
         const password = ']G_ce[24Ghd3'
         cy.visit('https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login')
         cy.contains('Username').type(username)
         cy.contains('Password').type(password)
         cy.get('form').submit()
+        cy.url().should('eq', 'https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Benefits');
+        cy.get('table').contains('td', 'Stacy').should('be.visible');
 
     });
 
-    it('creates new employee with entry over dependent max-expecting error', function () {
+    it('creates new employee and confirms they were added', function () {
         cy.contains('Add').click()
 
         cy.get('input[name=firstName]').type('Jason')
         cy.get('input[name=lastName]').type('Giddeon')
-        cy.get('input[name=dependants]').type('100')
+        cy.get('input[name=dependants]').type('3')
         cy.get('form').submit()
+        cy.get('table').contains('td', 'Jason').should('be.visible');
+
 
 
     });
